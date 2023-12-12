fdff9473f8442fe990a2af372361f5be2d1909e4
ChristieHyde <christieahyde@gmail.com>
2023-12-12 19:19:26 +1100

    installed Inquirer dependency package

--- index.js
+++ index.js
@@ -11,3 +11,23 @@ function init() {}
 
 // Function call to initialize app
 init();
+
+// SAMPLE INQUIRER USAGE
+// Source: npmjs.com/package/inquirer/v/8.2.4
+var inquirer = require('inquirer');
+inquirer
+  .prompt([
+    {
+        message: "type test message",
+    }
+  ])
+  .then((answers) => {
+    console.log(answers)
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      console.log("tty error");
+    } else {
+      console.log("error");
+    }
+  });
