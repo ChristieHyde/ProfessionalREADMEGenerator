e95feaa2d2aa88c5976b558cb3e48a589ec76597
ChristieHyde <christieahyde@gmail.com>
2023-12-14 21:11:42 +1100

    Added user input questions

--- index.js
+++ index.js
@@ -1,33 +1,93 @@
-// TODO: Include packages needed for this application
+// Package declaration
+var inquirer = require('inquirer');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+const DEBUG = false;
+
+// Questions for user input
+const questions = [
+    {
+        type: "input",
+        name: "title",
+        message: "Please enter the project title: "
+    },
+    {
+        type: "input",
+        name: "github_name",
+        message: "Please enter your GitHub username: "
+    },
+    {
+        type: "input",
+        name: "email_address",
+        message: "Please enter your email address: "
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Please enter a description of the project: "
+    },
+    {
+        type: "input",
+        name: "installation",
+        message: "Please enter the project's installation instructions: "
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "Please enter an overview of the project's usage: "
+    },
+    {
+        type: "input",
+        name: "contribution",
+        message: "Please enter the contribution guidelines for the project: "
+    },
+    {
+        type: "input",
+        name: "tests",
+        message: "Please enter the project's testing instructions: "
+    },
+    {
+        // TODO get list of licences
+        type: "list",
+        name: "licence",
+        message: "Please enter a description of the project: ",
+        choices: ["a", "b"]
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    // 
+    inquirer.prompt(questions)
+    .then(answers => {})
+    .catch(error => {})
+    
+}
 
 // Function call to initialize app
 init();
 
 // SAMPLE INQUIRER USAGE
 // Source: npmjs.com/package/inquirer/v/8.2.4
-var inquirer = require('inquirer');
-inquirer
-  .prompt([
-    {
-        message: "type test message",
-    }
-  ])
-  .then((answers) => {
-    console.log(answers)
-  })
-  .catch((error) => {
-    if (error.isTtyError) {
-      console.log("tty error");
-    } else {
-      console.log("error");
-    }
+if (DEBUG) {
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: "test",
+                message: "type test message",
+            }
+        ])
+        .then((answers) => {
+            console.log(answers)
+        })
+        .catch((error) => {
+            if (error.isTtyError) {
+            console.log("tty error");
+        } else {
+            console.log("error");
+        }
   });
+}
